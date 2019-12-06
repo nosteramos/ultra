c66e948e17ab7f5314c49447c40e6821b9638e15
yanoom <yanoom@gmail.com>
2019-12-06 22:17:15 +0200

    ..

--- Tests.py
+++ Tests.py
@@ -2,7 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-import TestsData
+from TestsData import TestData
 import page
 
 
@@ -12,23 +12,23 @@ class Tests(unittest.TestCase):
         options = Options()
         options.add_argument("--disable-notifications")
         self.driver = webdriver.Chrome("chromedriver.exe", options=options)
-        self.driver.get("https://www.facebook.com")
+        self.driver.get("https//facebook.com")
         login_page = page.LoginPage(self.driver)
         if login_page.is_displayed():
-            login_page.login(TestsData.TestData.FACEBOOK_USER, TestsData.TestData.FACEBOOK_PASSWORD)
+            login_page.login(TestData.FACEBOOK_USER, TestData.FACEBOOK_PASSWORD)
         home_page = page.HomePage(self.driver)
         assert home_page.is_logged_in()
 
     def test_facebook(self):
         home_page = page.HomePage(self.driver)
-        assert home_page.find(TestsData.TestData.MY_NAME)
+        assert home_page.find(TestData.MY_NAME)
 
-        home_page.show_profile() #
+        home_page.show_profile()
 
         profile_page = page.ProfilePage(self.driver)
         profile_page.navigate_to_about()  # Navigate to AND Verify the correct page is displayed
 
-        assert profile_page.birthday_found(TestsData.TestData.BIRTHDAY)
+        assert profile_page.birthday_found(TestData.BIRTHDAY)
 
         pass
 
---
+++ TestsData.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+class TestData:
+    MY_NAME = "Amos Mastbaum"  # For Step 1.c
+    FACEBOOK_USER = ""
+    FACEBOOK_PASSWORD = ""
+    BIRTHDAY = datetime(1976, 1, 17)
